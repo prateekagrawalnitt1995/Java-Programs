f338af4fd3a44b8106eeaa5ebf0d3a905cab57f5
prateekagrawalnitt1995 <prateekagrawalnitt1995@gmail.com>
2015-11-13 10:45:53 +0530

    Java Pratice Programs

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+class apples
+{
+ public static void main(String args[])
+        {
+            double pi;
+            pi= 3.14;
+            
+            System.out.print("Value of");
+            System.out.println("Pi is");
+            System.out.print(pi);
+        }       }
